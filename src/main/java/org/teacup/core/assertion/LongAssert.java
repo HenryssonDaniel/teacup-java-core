b3de20d57eb93b7c129fb7fff795d4a7b3fe8b84
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-03-06 12:28:01 -0600

    Add Long assertion support
    Added support for assertions with Long.

---
+++ src/main/java/org/teacup/core/assertion/LongAssert.java
@@ -0,0 +1,9 @@
+package org.teacup.core.assertion;
+
+/**
+ * Long assertions.
+ *
+ * @since 1.0
+ */
+public interface LongAssert
+    extends ComparableAssert<Long, LongAssert>, NumberAssert<Long, LongAssert> {}
---
+++ src/main/java/org/teacup/core/assertion/LongAssertImpl.java
@@ -0,0 +1,3 @@
+package org.teacup.core.assertion;
+
+class LongAssertImpl extends ComparableAssertImpl<Long, LongAssert> implements LongAssert {}
