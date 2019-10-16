0137cfa46f7d977ace6e1301d890cc1da77fd127
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-10-16 21:40:04 +0200

    Move Executor, Fixture and Setup
    Executor, Fixture and Setup is moved to a new
    repository together with it's implementations and tests.
    
    The files is part of the engine repository.

--- src/intTest/java/io/github/henryssondaniel/teacup/core/Constants.java
+++
@@ -1,9 +0,0 @@
-package io.github.henryssondaniel.teacup.core;
-
-enum Constants {
-  ;
-
-  static final Object CLIENT = new Object();
-  static final String CLIENT_NAME = "client";
-  static final String SERVER_NAME = "server";
-}
--- src/intTest/java/io/github/henryssondaniel/teacup/core/TestServer.java
+++
@@ -1,7 +0,0 @@
-package io.github.henryssondaniel.teacup.core;
-
-interface TestServer extends Server {
-  boolean isSetUp();
-
-  boolean isTearDown();
-}
